04186e3fed77374b8c518327bea7165633f330e8
Rob Schuler <robes@users.noreply.github.com>
2019-04-30 16:35:56 -0700

    wip: basic test of ermrest alter operation rule

--- test/catalog/test_ermrest.py
+++ test/catalog/test_ermrest.py
@@ -1,6 +1,8 @@
 import os
 import unittest
 
+from chisel.catalog.base import ComputedRelation
+from chisel.operators.base import Alter
 from .utils import ERMrestHelper, BaseTestCase
 
 ermrest_hostname = os.getenv('CHISEL_TEST_ERMREST_HOST')
@@ -15,3 +17,13 @@ class TestERMrestCatalog (BaseTestCase):
     def test_connect_setup(self):
         self.assertTrue(self._catalog is not None)
         self.assertTrue(self.catalog_helper.exists(self.catalog_helper.samples))
+
+    def test_alter(self):
+        with self._catalog.evolve() as ctx:
+            self._catalog['public'][self.catalog_helper.samples] =\
+                self._catalog['public'][self.catalog_helper.samples].select(
+                self.catalog_helper.FIELDS[1]
+            )
+            self.assertIsInstance(self._catalog['public'][self.catalog_helper.samples], ComputedRelation)
+            self.assertIsInstance(self._catalog['public'][self.catalog_helper.samples]._physical_plan, Alter)
+            ctx.abort()
